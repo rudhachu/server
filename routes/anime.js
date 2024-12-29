e67d7307c9adca5d874b8a42f43329c83bb77d6a
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-12-29 19:45:56 +0530

    Update anime.js

--- routes/anime.js
+++ routes/anime.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { AnimeNews } from '../utils/anime.js';
+const express = require('express');
+const { AnimeNews } = require('../utils/anime.js');
 
 const router = express.Router();
 
 router.get('/animenews', async (req, res) => {
-	try {
-		const response = await AnimeNews();
-		res.json(response);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+    try {
+        const response = await AnimeNews();
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-export default router
+module.exports = router;
